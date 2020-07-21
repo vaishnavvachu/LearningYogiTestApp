33d39bac9fe644fae04ceb91232d450e5543f8dc
Vaishnav Viswanath <vaishnavviswan@gmail.com>
2020-07-21 15:16:33 +0530

    Add ObstacleSpawner

---
+++ LearningYogiTestApp/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    private float timeBetweenSpawns;
+    public float initialTimeBetweenSpawns;
+    public float timeDecrease;
+    public float minTime;
+
+    public GameObject[] obstaclesPrefabs;
+
+    private void Start()
+    {
+        timeBetweenSpawns = initialTimeBetweenSpawns;
+    }
+
+    private void Update()
+    {
+        if (timeBetweenSpawns <= 0)
+        {
+            int rand = Random.Range(0, obstaclesPrefabs.Length);
+            Instantiate(obstaclesPrefabs[rand], transform.position, Quaternion.identity);
+            timeBetweenSpawns = initialTimeBetweenSpawns;
+            if (initialTimeBetweenSpawns > minTime)
+            {
+                initialTimeBetweenSpawns -= timeDecrease;
+            }
+        }
+        else
+        {
+            timeBetweenSpawns -= Time.deltaTime;
+        }
+    }
+
+}
